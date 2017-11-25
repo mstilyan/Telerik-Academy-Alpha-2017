47357eb914e4cf5fc34c1294a1139dd06c31d401
Stilyan Mladenov <spmladenov@sofia-uni.bg>
2017-11-25 14:44:34 +0200

    Added solution to problems: MaximalSum SequenceInMatrix LargestAreaInMatrix

---
+++ MultidimentionalArrays/LargestAreaInMatrix/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestAreaInMatrix
+{
+    class Program
+    {
+        private static int rows;
+        private static int cols;
+        private static bool[,] used;
+        private static int currArea;
+        private static int largestArea;
+
+        static void Main(string[] args)
+        {
+            var token = Console.ReadLine().Split().Select(int.Parse);
+            rows = token.First();
+            cols = token.Last();
+
+            var matrix = new int[rows][];
+            used = new bool[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                matrix[row] = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (!used[row, col])
+                    {
+                        currArea = 0;
+                        DFS(matrix, row, col, matrix[row][col]);
+                    }
+                }
+            }
+
+            Console.WriteLine(largestArea);
+        }
+
+        private static void DFS(int[][] matrix, int row, int col, int value)
+        {
+            if (row < 0 || row >= rows || col < 0 || col >= cols || matrix[row][col] != value || used[row, col])
+                return;
+
+            currArea++;
+            used[row, col] = true;
+            largestArea = largestArea < currArea ? currArea : largestArea;
+            //Console.WriteLine($"{currArea} ({row}, {col})");
+
+            DFS(matrix, row, col + 1, value);
+            DFS(matrix, row, col - 1, value);
+            DFS(matrix, row + 1, col, value);
+            DFS(matrix, row - 1, col, value);
+        }
+    }
+}
