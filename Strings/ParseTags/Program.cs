a41401564d70ab996c6ceb763aa98098b81c5c1d
Stilyan Mladenov <spmladenov@sofia-uni.bg>
2017-11-28 07:35:16 +0200

    Added solution to String Problems: CorrectBrackets, ParseTags, ReverseString, StringLenght. SubstringInText and ExtractSentences task in progress.

---
+++ Strings/ParseTags/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ParseTags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Console.WriteLine(Regex.Replace(input, @"<upcase>(.*?)</upcase>", m => m.Groups[1].Value.ToUpper()));
+        }
+    }
+}
